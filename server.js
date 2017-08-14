87a67a72b5e79aaa4fb24a92f80ebdc0a603330c
Kamon Smith <Kamon@Kamons-MacBook-Pro.local>
2017-08-15 01:08:12 +0700

    add allow cross domain headers

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 /*----------  setup all the tool we need  ----------*/
 
-var express 	= require('express');
-var _ 			= require('lodash');
-var mongoose 	= require('mongoose');
-var bodyParser 	= require('body-parser');
-var morgan 		= require('morgan');
+let express 	= require('express');
+let mongoose 	= require('mongoose');
+let bodyParser 	= require('body-parser');
+let morgan 		= require('morgan');
 
 /*----------  Load Config  ----------*/
 
-var PORT 		= process.env.PORT || 3000
-var configDB 	= require('./api/configs/database.js');
-var configJWT 	= require('./api/configs/jwt.js');
+const PORT 		= process.env.PORT || 3000
+const configDB 	= require('./api/configs/database.js');
+const configJWT 	= require('./api/configs/jwt.js');
 
 
 /*----------  Configuration  ----------*/
 var app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") 
+        res.send(200);
+    else 
+        next();
+});
+
 app.use(express.static('public'))
+
 
 app.set('superDuperSecret',configJWT.secret);
 
@@ -33,24 +43,21 @@ app.use(bodyParser.urlencoded({
 
 /*----------  Import Routes ----------*/
 
-var indexRoute 	= require('./api/routes/index');
-var authRoute 	= require('./api/routes/auth');
-var usersRoute 	= require('./api/routes/users');
+const indexRoute 	= require('./api/routes/index');
+const authRoute 	= require('./api/routes/auth');
+const usersRoute 	= require('./api/routes/users');
+const companyProfileRoute = require('./api/routes/company');
 
 
 /**
-
 	TODO:
 	- Remove the setup Routes
-
  */
 
-var setupInitData = require('./api/setup/setupInitData')
+const setupInitData = require('./api/setup/setupInitData')
 app.get('/setup', setupInitData.setupInitData);
 
 /*----------  Remove above  ----------*/
-
-
 
 
 /*----------  Add Routes here  ----------*/
@@ -62,8 +69,16 @@ app.use('/auth',authRoute)
 // Restrict Routs List
 app.use('/*', require('./api/middlewares/authValidate.js'));
 app.use('/manage-user',usersRoute);
+app.use('/manage-company',companyProfileRoute);
 
-//appxxx other
+
+/**
+
+	TODO:
+	- Add another route here!
+
+ */
+
 
 app.listen(PORT, () => {
     console.log('ATM-MNT Started on : http://localhost:' + PORT);
